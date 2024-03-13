e4bb10894ebc064f5529d19b596fcb1b7ea03caf
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-03-13 14:54:10 +0100

    3. Optional - solutions 1/2

--- 01-intro/src/main/java/com/for_comprehension/function/E02/Optionals.java
+++ 01-intro/src/main/java/com/for_comprehension/function/E02/Optionals.java
@@ -18,34 +18,37 @@ class Optionals {
     /**
      * Call {@link this#findOneById } with a provided id and handle absence by throwing {@link NoSuchElementException}
      * Hint: {@link Optional#get()}
-     *
+     * <p>
      * WARNING: this is an anti-pattern (unless you know what you're doing)
      */
     static Function<Integer, Person> L1_bruteForceGet() {
-        return todo();
+        return i -> findOneById(i).orElseThrow();
     }
 
     /**
      * Call {@link this#findOneById } with a provided id and handle absence by throwing {@link IllegalStateException}
      * Hint: check {@link Optional}'s API
-     *
      */
     static Function<Integer, Person> L2_customException() {
-        return todo();
+        return i -> findOneById(i).orElseThrow(IllegalStateException::new);
     }
 
     /**
      * Get {@link Person#name} if found or else return {@link this#DEFAULT}
      */
     static Function<Integer, String> L3_defaultValue() {
-        return todo();
+        return i -> findOneById(i)
+          .map(Person::getName)
+          .orElse(DEFAULT);
     }
 
     /**
      * Get {@link Person#name} if found or else return the value returned by provided method (represented by Supplier<String)
      */
     static BiFunction<Integer, Supplier<String>, String> L4_defaultValueMethodResult() {
-        return todo();
+        return (i, supplier) -> findOneById(i)
+          .map(Person::getName)
+          .orElseGet(supplier);
     }
 
     /**
@@ -55,7 +58,11 @@ class Optionals {
      * Hint: {@link Optional#filter}
      */
     static Function<Integer, String> L5_processValue() {
-        return todo();
+        return i -> findOneById(i)
+          .map(p -> p.getName())
+          .filter(name -> !name.isBlank())
+          .map(name -> name.toUpperCase())
+          .orElse(DEFAULT);
     }
 
     /**
@@ -66,7 +73,11 @@ class Optionals {
      * Hint: {@link Optional#flatMap(Function)}
      */
     static Function<Integer, Integer> L6_nestedOptionals() {
-        return todo();
+        return i -> findOneById(i)
+          .map(Person::getName)
+          .filter(name -> !name.isBlank())
+          .flatMap(Optionals::findAgeByName)
+          .orElse(42);
     }
 
     static class Person {
