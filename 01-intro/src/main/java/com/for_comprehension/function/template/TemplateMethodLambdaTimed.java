b47e732f051f35cbf33e1ee746875f52fd80aa86
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-03-13 16:15:08 +0100

    4. TemplateMethod - solution

--- 01-intro/src/main/java/com/for_comprehension/function/template/TemplateMethodLambdaTimed.java
+++ 01-intro/src/main/java/com/for_comprehension/function/template/TemplateMethodLambdaTimed.java
@@ -2,17 +2,43 @@ package com.for_comprehension.function.template;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
 class TemplateMethodLambdaTimed {
 
     public static void main(String[] args) {
-        int result = timed(() ->calculate());
-        // w logach: duration: XXX ms
+        var result = timed(() -> calculate());
+        System.out.println("result: " + result);
+
+        var result2 = timed(() -> "foo");
+        System.out.println("result2: " + result2);
     }
 
-    public static ??? timed(??? action) {
-        // ???
+    public static <T> T timed(Supplier<T> action) {
+        var before = Instant.now();
+        T result = action.get();
+        var after = Instant.now();
+
+        System.out.println("duration: " + Duration.between(before, after).toNanos() + "ns");
+        return result;
+    }
+
+    public static <T> Optional<T> timedUnchecked(Callable<T> action) {
+        var before = Instant.now();
+        T result = null;
+        try {
+            result = action.call();
+        } catch (Exception e) {
+            System.out.println("duration: " + Duration.between(before, Instant.now()).toNanos() + "ns");
+            return Optional.empty();
+        }
+
+        System.out.println("duration: " + Duration.between(before, Instant.now()).toNanos() + "ns");
+
+        return Optional.ofNullable(result);
     }
 
     public static void runWithLogging(Runnable action) {
