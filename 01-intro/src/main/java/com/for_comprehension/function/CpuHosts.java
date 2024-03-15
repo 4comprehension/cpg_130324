4fec5b3509a363135bae5e29f7858a8f5cec5002
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-03-15 12:10:36 +0100

    15. ForkJoin

---
+++ 01-intro/src/main/java/com/for_comprehension/function/CpuHosts.java
@@ -0,0 +1,8 @@
+package com.for_comprehension.function;
+
+class CpuHosts {
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
--- 01-intro/src/main/java/com/for_comprehension/function/L4_StreamParallelSaturated.java
+++ 01-intro/src/main/java/com/for_comprehension/function/L4_StreamParallelSaturated.java
@@ -4,11 +4,28 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-class L4_StreamParallel {
+class L4_StreamParallelSaturated {
 
     public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            IntStream.range(0, 20).boxed()
+              .parallel()
+              .map(i -> {
+                  try {
+                      Thread.sleep(1000000);
+                  } catch (InterruptedException e) {
+                      throw new RuntimeException(e);
+                  }
+
+                    return i;
+              }).toList();
+        }).start();
+
+        Thread.sleep(1000);
+
         List<Integer> r = timed(() -> Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
           .parallel()
           .map(i -> process(i))
